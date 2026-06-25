d023e20370ccdd33aff758ff05216a45e9f3ed4b
Salifu Yakubu <salifuyakubu333@gmail.com>
2026-06-25 09:41:33 +0000

    mining some blocks in step 4 for step 5 to pass.

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,4 @@
 # Create a wallet with the name "builderswallet".
 bitcoin-cli -regtest createwallet "builderswallet"
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 $ADDR
